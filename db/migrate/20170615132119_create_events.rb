ea754455d1ccc4b366b376b1707dff226ffe44dd
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-15 08:47:02 -0500

    begin defining migrations

--- db/migrate/20170615132119_create_events.rb
+++ db/migrate/20170615132119_create_events.rb
@@ -1,7 +1,9 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
-
+	t.belongs_to :volunteer, index: true
+      t.belongs_to :customer, index: true
+      t.datetime :event_date
       t.timestamps
     end
   end
